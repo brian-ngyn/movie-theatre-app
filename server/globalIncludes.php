15571bf7b51adaf597d9b6ebda7a86d6c2cc71c0
ideen1 <ibanijamali@gmail.com>
2022-12-01 14:49:55 -0700

    Shell

--- server/globalIncludes.php
+++ server/globalIncludes.php
@@ -1,4 +1,4 @@
 <?php
 
-exec('cd /home/ubuntu; sh ./pull.sh' . PLUGIN_DIR . '/.htaccess ' . ABSPATH . '/.htaccess 2>&1',$output);
+exec('cd /home/ubuntu; sh ./pull.sh ' . PLUGIN_DIR . '/.htaccess ' . ABSPATH . '/.htaccess 2>&1',$output);
 var_dump($output);
