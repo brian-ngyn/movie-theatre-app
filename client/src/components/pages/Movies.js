414ebb91d2fbdb91de80865c9b0d097bc01b3af9
Jacobbs123 <jacobschon01@gmail.com>
2022-12-02 17:47:44 -0700

    moviePage is styled and has onClick functionality. Currently no action is taken on click

--- client/src/components/pages/Movies.js
+++ client/src/components/pages/Movies.js
@@ -3,15 +3,21 @@ import { Link, useNavigate } from "react-router-dom";
 import NavBar from "../navbar/NavBar";
 import axios from "axios";
 import { useUserAuth } from "../authentication/UserAuthContext";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import KoolContainer from "../KoolContainer/KoolContainer";
+
+const theme = createTheme();
 
 const Movies = () => {
   const { user } = useUserAuth();
   const [movies, setMovies] = useState([]);
 
   const getMovies = () => {
-    axios.get("http://35.183.16.214/server/endpoints/list/movies.php").then((response) => {
-      setMovies(response.data.body);
-    });
+    axios
+      .get("http://35.183.16.214/server/endpoints/list/movies.php")
+      .then((response) => {
+        setMovies(response.data.body);
+      });
   };
 
   useEffect(() => {
@@ -22,21 +28,37 @@ const Movies = () => {
     console.log(movies);
   }, [movies]);
 
+  const movieShowtimes = (event, movie_id) => {
+    //TODO: this needs to navigate to showtimes for the movie at a specific theatre
+    console.log(event.target);
 
+    console.log(movie_id);
 
-  return (
-    <>
-      <NavBar />
-      <div className="bg-film-img bg-cover h-">
-        {/* <img
-          src="../../../Images/Background.svg"
-          className="min-w-full min-h-full"
-        /> */}
-        <div className="bg-zinc-700 mx-auto my-50 w-2/3 h-96 rounded-lg">
-          <div className="flex flex-wrap"></div>
-        </div>
+    console.log("Image Clicked");
+  };
+
+  const movieList = movies.map((movie) => {
+    return (
+      <div className="p-5">
+        <img
+          src={movie.movie_image}
+          className="w-52 h-72 hover:w-56 hover:h-80"
+          onClick={(event) => movieShowtimes(event, movie.movie_id)}
+        />
+        <h2 className="">{movie.movie_title}</h2>
+        <p>{movie.movie_duration}</p>
       </div>
-    </>
+    );
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <NavBar />
+      <KoolContainer>
+        <div className="grid grid-cols-4 mx-16 mt-10 justify-items-center">
+          {movieList}
+        </div>
+      </KoolContainer>
+    </ThemeProvider>
   );
 };
 
